4813e124765214ce2d81e7de064351ec7448709d
elvin-gts <emdd007@gmail.com>
2024-04-21 08:57:56 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser";
 import dotenv from 'dotenv';
 
 dotenv.config();
-const port=process.env.PORT || 3000;
+const port=process.env.PORT || 5000;
 
 const app = express();
 
