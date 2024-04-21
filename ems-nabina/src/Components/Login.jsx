c8d0b6da5983a0d42e907a51fc1c7b286f430931
elvin-gts <emdd007@gmail.com>
2024-04-21 11:08:49 +0300

    Update Login.jsx

--- ems-nabina/src/Components/Login.jsx
+++ ems-nabina/src/Components/Login.jsx
@@ -3,7 +3,10 @@ import './style.css'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 
+
+
 const Login = () => {
+
     const [values, setValues] = useState({
         email: '',
         password: '' 
@@ -21,7 +24,10 @@ const Login = () => {
                     localStorage.setItem("valid", true)
                     const userid = result.data.id;
                     localStorage.setItem("userid", userid)
+                    console.log(userid)
                     if(result.data.role === "ADMIN") {
+                        console.log("admin entering")
+                        console.log(userid)     /* TEstting */
                         localStorage.setItem("adminid", userid)
                         navigate('/dashboard', { state: { adminid: userid } })
                     } else if(result.data.role === "HR") {
@@ -37,32 +43,27 @@ const Login = () => {
             })
             .catch(err => console.log(err))
     }
-
-    const handleCheckboxClick = () => {
-        document.getElementById('tick').checked = !document.getElementById('tick').checked;
-    }
-
-    return (
-        <div className='container-fluid d-flex justify-content-center align-items-center vh-100 loginPage'>
-            <div className='p-3 rounded w-75 w-md-50 w-lg-25 border loginForm'>
-                <div className='text-warning'>
-                    {error && error}
+  return (
+    <div className='d-flex justify-content-center align-items-center vh-100 loginPage'>
+        <div className='p-3 rounded w-25 border loginForm'>
+            <div className='text-warning'>
+                {error && error}
+            </div>
+            <h2>Login Page</h2>
+            <form onSubmit={handleSubmit}>
+                <div className='mb-3'>
+                    <label htmlFor="email"><strong>Email:</strong></label>
+                    <input type="email" name='email' autoComplete='off' placeholder='Enter Email'
+                     onChange={(e) => setValues({...values, email : e.target.value})} className='form-control rounded-0'/>
                 </div>
-                <h2>Login Page</h2>
-                <form onSubmit={handleSubmit}>
-                    <div className='mb-3'>
-                        <label htmlFor="email"><strong>Email:</strong></label>
-                        <input type="email" name='email' autoComplete='off' placeholder='Enter Email'
-                            onChange={(e) => setValues({...values, email : e.target.value})} className='form-control rounded-0'/>
-                    </div>
-                    <div className='mb-3'> 
-                        <label htmlFor="password"><strong>Password:</strong></label>
-                        <input type="password" name='password' placeholder='Enter Password'
-                            onChange={(e) => setValues({...values, password : e.target.value})} className='form-control rounded-0'/>
-                    </div>
-                    <button className='btn btn-success w-100 rounded-0 mb-2'>Log in</button>
-                </form>
-                <div className="text-center mb-1" onClick={handleCheckboxClick} style={{cursor: 'pointer'}}>
+                <div className='mb-3'> 
+                    <label htmlFor="password"><strong>Password:</strong></label>
+                    <input type="password" name='password' placeholder='Enter Password'
+                     onChange={(e) => setValues({...values, password : e.target.value})} className='form-control rounded-0'/>
+                </div>
+                <button className='btn btn-success w-100 rounded-0 mb-2'>Log in</button>
+                <div className="text-center">
+                <div className='mb-1'>
                     <input
                         type="checkbox"
                         name="tick"
@@ -70,14 +71,16 @@ const Login = () => {
                         className='me-2'
                         required
                     />
-                    <label htmlFor="tick">Agree with terms & conditions</label>
+                    <label htmlFor="password">Agree with terms & conditions</label>
                 </div>
-            </div>
-            <button type="button" className="btn btn-primary px-5 py-3 mt-3" onClick={() => navigate('/')}>
+</div>
+            </form>
+            <button type="button" className="btn btn-primary px-5 py-3 mt-3 mx-auto" onClick={() => navigate('/')}>
                 Main Menu
             </button>
         </div>
-    )
+    </div>
+  )
 }
 
 export default Login
