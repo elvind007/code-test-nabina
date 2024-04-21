81058633385754297e7478ee39859eb7e71ed81b
elvin-gts <emdd007@gmail.com>
2024-04-21 09:20:10 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,25 +6,17 @@ import cookieParser from "cookie-parser";
 import dotenv from 'dotenv';
 
 dotenv.config();
-const port=process.env.PORT || 5000;
-
-const app = express();
+const port=process.env.PORT || 3000;
 
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || '(c+&76-aewt97-&(-$f!pbj+xmgua+p02qo%j&0a!k=318=iga';
 
-app.use((req, res, next) => {
-    const allowedOrigins = ["https://code-test-nabina-vercel.app", "https://code-test-nabina-git-main-elvind007s-projects.vercel.app", "https://code-test-nabina-g9old3drz-elvind007s-projects.vercel.app"];
-    const origin = req.headers.origin;
-
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-});
+const app = express() 
+app.use(cors({
+    origin: ["https://code-test-nabina.vercel.app","https://code-test-nabina-git-main-elvind007s-projects.vercel.app", "https://code-test-nabina-g9old3drz-elvind007s-projects.vercel.app" ],
+    methods: ['GET', 'POST', 'PUT', "DELETE"],
+    credentials: true,
+    preflightContinue: true  // Allow preflight requests to pass through
+}))
 
 app.use(express.json())
 app.use(cookieParser())
@@ -49,11 +41,10 @@ const verifyUser = (req, res, next) => {
         return res.json({Status: false, Error: "Not authenticated"});
     }
 }
-
 app.get('/verify',verifyUser, (req, res)=> {
     return res.json({Status: true, role: req.role, id: req.id});
-});
+} );
 
 app.listen(port,"0.0.0.0", () => {
     console.log("Server is running")
-});
+})
